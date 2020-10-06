a0577c8992624cb43c73e10310760d6a068410bd
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-10-06 22:15:06 +0300

    Updates

---
+++ precache-manifest.fadc396ed9e0a5cbaa5ed5753764ab5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ec6c607512481f7cc2a9b39a61a793",
+    "url": "/genesis-millionaire/index.html"
+  },
+  {
+    "revision": "b414efc0209fda0a7f18",
+    "url": "/genesis-millionaire/static/css/main.f2c9bbec.chunk.css"
+  },
+  {
+    "revision": "3f02941ffccf82559056",
+    "url": "/genesis-millionaire/static/js/2.7b6abbcc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/genesis-millionaire/static/js/2.7b6abbcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b414efc0209fda0a7f18",
+    "url": "/genesis-millionaire/static/js/main.659c475c.chunk.js"
+  },
+  {
+    "revision": "970622571da11aef4aa5",
+    "url": "/genesis-millionaire/static/js/runtime-main.8d87d6df.js"
+  }
+]);
