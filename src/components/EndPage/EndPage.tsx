10b1baf0daefcb7b585eafa67bdb0174fa9f9d41
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-10-06 15:02:19 +0300

    End game screen

--- src/components/EndPage/EndPage.tsx
+++ src/components/EndPage/EndPage.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { StartGameIcon } from '../../assets';
+import Button from '../Button/Button';
 import './EndPage.css';
 
 function EndPage() {
   return (
-    <div className="">
-     <span>EndGame</span>
+    <div className="end-page-container column column-h column-v">
+      <div className="row">
+        <span className="logo">{StartGameIcon}</span>
+        <div className="column column-v start-text-and-button">
+          <span className="score-text">Total score:</span>
+          <span className="earned-text">$8,000 earned</span>
+          <span className="start-game-button">
+            <Button buttonText={"Try again"}></Button>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
