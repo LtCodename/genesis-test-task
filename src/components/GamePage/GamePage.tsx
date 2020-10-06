79e2014fd664cf0c9eede6983db46b9de539ac25
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-10-06 20:02:14 +0300

    Mobile css

--- src/components/GamePage/GamePage.tsx
+++ src/components/GamePage/GamePage.tsx
@@ -5,6 +5,7 @@ import { useHistory } from "react-router-dom";
 import { Context } from '../../App';
 import AnswerButton from '../AnswerButton/AnswerButton';
 import RewardItem from '../RewardItem/RewardItem';
+import { CloseIcon, MenuIcon } from '../../assets';
 
 interface IQuestion {
   question: string;
@@ -36,9 +37,26 @@ function GamePage() {
   const [questions, setQuestions] = useState(initialQuestions);
   const [reward, setReward] = useState(0);
 
+  const [bothSidesWrapperClass, setBothSidesWrapperClass] = useState("row question-and-progress row-between");
+  const [leftPartClass, setLeftPartClass] = useState("question column");
+  const [rightPartClass, setRightPartClass] = useState("question-tree column column-v column-h");
+  const [menuType, setMenuType] = useState("burger");
+
   useEffect(() => {
     setQuestions(questionsData);
   },[]);
+
+  function showQuestionsTree() {
+    setLeftPartClass("question column question-forced-hide");
+    setRightPartClass("question-tree column column-v column-h question-tree-forced-show");
+    setMenuType("close");
+  }
+
+  function hideQuestionsTree() {
+    setLeftPartClass("question column");
+    setRightPartClass("question-tree column column-v column-h");
+    setMenuType("burger");
+  }
 
   function handleAnswer(answerIndex:number) {
 
@@ -93,16 +111,17 @@ function GamePage() {
   
   return (
     <div className="game-page-container column column-h column-v">
-      <div className="row question-and-progress row-between">
-        <div className="question column">
+      <div className={bothSidesWrapperClass}>
+        <div className={leftPartClass}>
           <span className="question-text">{questions.length && questions[questionIndex] ? questions[questionIndex].question : "Error occured!"}</span>
           <div className="row answers-row">
             {answers}
           </div>
         </div>
-        <div className="question-tree column column-v column-h">
+        <div className={rightPartClass}>
           {rewardItems}
         </div>
+        {menuType === "burger" ? <span className="show-menu-icon" onClick={showQuestionsTree}>{MenuIcon}</span> : <span className="show-menu-icon" onClick={hideQuestionsTree}>{CloseIcon}</span>}
       </div>
     </div>
   );
